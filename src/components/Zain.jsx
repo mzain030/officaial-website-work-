a83de0efd1ade6e1009968429f4a651ec130ad56
mzain030 <113288311+mzain030@users.noreply.github.com>
2025-02-26 11:39:59 +0500

    tasks s

--- src/components/Zain.jsx
+++ src/components/Zain.jsx
@@ -7,6 +7,17 @@ const Zain = () => {
         <button>Zain Naseer Click</button>
         <h1>i love pakistan </h1>
         <h2>i love codding</h2>
+        ali husnain task 
+        usestate ki ek example kr ka aani h apni file ma or git per push krni h 
+        {/* /// */}
+        raoouf task
+        props ki ek example kr ka aani h apni file ma or git per push krni h 
+        {/* ////  */}
+        bilal task 
+        useEffect ma counter bana kr ek example kr ka aani h apni file ma or git per push krni h 
+        {/* ///  */}
+        junaidkhan task 
+        usecontext kr ka aana h apni file ma or git per push krna h  
     </div>
   )
 }
