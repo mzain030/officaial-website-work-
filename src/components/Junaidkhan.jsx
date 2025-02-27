597b9304f8a9e3b2425cf5f3e726e55281aaa6a2
junaidarshad1122 <jj8286014@gmail.com>
2025-02-27 10:17:30 -0800

    junaid khan

--- src/components/Junaidkhan.jsx
+++ src/components/Junaidkhan.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import mytask from './conetxt/Contextr'
+import JunaidKhany from './JunaidKhany'
 const Junaidkhan = () => {
+const newobject={
+  first:"junaid",
+  last:"arshad",
+  age:20,
+  address:"pattoki"
+}
+
   return (
     <div>
       <input>Junaidkhan</input>
+    <mytask.Provider value={newobject}>
+      <JunaidKhany/>
+    </mytask.Provider>
+   
     </div>
   )
 }
---
+++ src/components/conetxt/Contextr.js
@@ -0,0 +1,5 @@
+import { createContext,useContext } from "react";
+
+const mytask = createContext();
+
+export default mytask;
