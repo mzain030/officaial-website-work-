f39a1d8d58515b3cd9b8906b35e8b4088e92c9b8
bilalansar3 <doctorofseoandcodding@gmail.com>
2025-02-27 11:31:19 +0500

    add

--- src/components/Pracitce.jsx
+++ src/components/Pracitce.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 
 const Pracitce = () => {
-<<<<<<< HEAD
 const [data,setData]=useState("")
 
   return (
@@ -9,14 +8,6 @@ const [data,setData]=useState("")
       <h1>Bilal Ansar</h1>
       <h4>{data}</h4>
       <button onClick={()=>setData("you have clicked button")}>Click me to show data</button>
-=======
-    const {hi,setHi}=useState("");
-  return (
-    <div>
-      <h1>JUNAID</h1>
-      <button onClick={(()=>setHi())}> Click Me </button>
-      <h2>{hi}</h2>
->>>>>>> 948859e54e1087d3fd3845fa91d04a62a4c45af6
     </div>
   )
 }
