4e23bffbd71ec0a26314151d356ae3ba2caef457
bilalansar3 <doctorofseoandcodding@gmail.com>
2025-02-27 11:07:09 +0500

    billa

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import Zain from './components/Zain';
 import Junaidkhan from './components/Junaidkhan';
 import Bilal from './components/Bilal';
 import Ali from './components/Ali';
+import Pracitce from './components/Pracitce';
+
 
 function App() {
   return (
     <div className="App">
-      <Junaidkhan/>
+      {/* <Junaidkhan/> */}
      <Zain/>
      <Bilal/>
      <Ali/>
-     <Practice
+     <Pracitce/>
     </div>
   );
 }
--- src/components/Pracitce.jsx
+++ src/components/Pracitce.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Pracitce = () => {
+const [data,setData]=useState("")
+
   return (
     <div>
-      
+      <h1>Bilal Ansar</h1>
+      <h4>{data}</h4>
+      <button onClick={()=>setData("you have clicked button")}>Click me to show data</button>
     </div>
   )
 }
